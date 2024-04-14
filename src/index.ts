0d7fd8ce35637728d8dcfd765514e988efdf6edc
mabanana <aldwin.yu@hotmail.com>
2024-04-14 19:09:42 -0400

    Setup echo bot

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,58 @@
-import { HandleRequest, HttpRequest, HttpResponse, Config } from "@fermyon/spin-sdk"
+import {
+  HandleRequest,
+  HttpRequest,
+  HttpResponse,
+  Config,
+} from "@fermyon/spin-sdk";
 
-export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
-  const sec = Config.get("secret")
-  const response = await fetch("https://develop.games", { redirect: "follow" })
-  const text = await response.text()
-  
+const TELEGRAM_API_URL = "https://api.telegram.org";
+
+interface TelegramUpdate {
+  message?: {
+    text?: string;
+    chat: {
+      id: number;
+    };
+  };
+}
+
+async function sendTextMessage(text: string, chatId: number, botToken: string) {
+  return fetch(TELEGRAM_API_URL + "/bot" + botToken + "/sendMessage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: text,
+    }),
+  });
+}
+
+export const handleRequest: HandleRequest = async function (
+  request: HttpRequest
+): Promise<HttpResponse> {
+  const botToken = Config.get("telegram_bot_token");
+  const body: TelegramUpdate = request.json();
+
+  if (body.message === undefined) {
+    return {
+      status: 200,
+    };
+  }
+  const chatId = body.message.chat.id;
+
+  if (body.message.text === undefined) {
+    await sendTextMessage("Message text not found.", chatId, botToken);
+    return {
+      status: 200,
+    };
+  }
+  const message = body.message.text;
+
+  await sendTextMessage(message + " from bot", chatId, botToken);
+
   return {
     status: 200,
-    headers: { "content-type": "text/html; charset=utf-8" },
-    body: text
-  }
-}
+  };
+};
